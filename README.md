788a39da2dacff7f2d79ea3322b3f649a74e8f77
Owen Buckley <obuckley@kenzan.com>
2017-05-22 23:03:01 -0400

    Initial setup (#1)
    * project files
    
    * init commit
    
    * up and running
    
    * add build script
    
    * links

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # alexa-skill-analog-studios
 An Alexa skill for getting information from the Analog Studios website
+
+## Links / References
+
+- [https://developer.amazon.com/edw/home.html#/](https://developer.amazon.com/edw/home.html#/)
+- [https://github.com/alexa/alexa-skills-kit-sdk-for-nodejs](https://github.com/alexa/alexa-skills-kit-sdk-for-nodejs)
---
+++ src/index.js
@@ -0,0 +1,53 @@
+const Alexa = require('alexa-sdk');
+
+const APP_ID = '';
+const SKILL_NAME = 'Analog Studios';
+const NAMES = [
+  'Dave',
+  'Owen'
+];
+
+function getName() {
+  let nameIndex = Math.floor(Math.random() * NAMES.length);
+  let name = NAMES[nameIndex];
+
+  return name;
+}
+
+exports.handler = function(event, context) {
+  let alexa = Alexa.handler(event, context);
+
+  alexa.APP_ID = APP_ID;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
+
+let handlers = {
+  'LaunchRequest': function () {
+    this.emit('GetMessage');
+  },
+  'Unhandled': function () {
+    this.emit('AMAZON.HelpIntent');
+  },
+  'GetNewMessageIntent': function () {
+    this.emit('GetMessage');
+  },
+  'GetMessage': function () {
+    let name = getName();
+    let speechOutput = `Hi ${name}!  This message sent from Analog Studios.`;
+
+    this.emit(':tellWithCard', speechOutput, SKILL_NAME, name);
+  },
+  'AMAZON.HelpIntent': function () {
+    let speechOutput = 'You can say just my invocation word for a message or, you can say exit...';
+    let reprompt = 'What can I help you with?';
+
+    this.emit(':ask', speechOutput, reprompt);
+  },
+  'AMAZON.CancelIntent': function () {
+    this.emit(':tell', 'Goodbye!');
+  },
+  'AMAZON.StopIntent': function () {
+    this.emit(':tell', 'Goodbye!');
+  }
+};
